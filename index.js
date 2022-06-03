957ae3554f11535df5d42ffe0db9545531cbfcb9
Jason Bracco <bracco.jason@gmail.com>
2022-06-03 17:14:16 -0400

    commit tree b3e2b0d5179f31e8d20d1b75641915502ebf2f7f

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-/* Your Code Here */
-
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
-
-const allWagesFor = function () {
+function createEmployeeRecord(array){
+    let timeInArray = []
+    let timeOutArray = []
+    let singleEmployeeRecord ={
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: timeInArray,
+        timeOutEvents: timeOutArray,
+    }
+    return singleEmployeeRecord
+}
+let allWagesFor = function(){
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
@@ -20,4 +22,105 @@ const allWagesFor = function () {
 
     return payable
 }
+
+console.log(singleEmployeeRecord.showRecord())
+function createEmployeeRecords(arrayOfArrays){
+    const employeeRecords = arrayOfArrays.map(element => createEmployeeRecord(element))
+    return employeeRecords
+}
+
+function createTimeInEvent(dateString){
+    let timeInDateHourArray = dateString.split(" ")
+    let date = timeInDateHourArray[0]
+    let hour = timeInDateHourArray[1]
+    let timeInObject = {
+        type: "TimeIn",
+        hour: parseInt(hour),
+        date: date
+    }
+    this.timeInEvents.push(timeInObject)
+    return this
+}
+
+function createTimeOutEvent(dateString){
+    let timeOutDateHourArray = dateString.split(" ")
+    let date = timeOutDateHourArray[0]
+    let hour = timeOutDateHourArray[1]
+    let timeOutObject = {
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date: date
+    }
+    this.timeOutEvents.push(timeOutObject)
+    return this
+}
+
+function hoursWorkedOnDate(dateString){
+    for(let i=0;i<this.timeInEvents.length;i++) {
+        let dateTimeClockedIn = this.timeInEvents[i].hour
+        let dateTimeClockedOut = this.timeOutEvents[i].hour
+        if (dateString === this.timeInEvents[i].date){
+            return((dateTimeClockedOut-dateTimeClockedIn)/100)
+        }
+    }
+}
+
+function wagesEarnedOnDate(dateString){
+    let payRate = this.payPerHour
+    let hours = hoursWorkedOnDate.call(this, dateString)
+    return hours*payRate
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+    let employee = srcArray.find(element => {
+            if (element.firstName === firstName){
+                return element
+            }
+        })
+    return employee
+}
+
+function calculatePayroll(array){    
+    let initialValue = 0
+    let fullPay = array.reduce((previousValue, currentValue) => previousValue + allWagesFor.call(currentValue), initialValue)
+    return fullPay
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
